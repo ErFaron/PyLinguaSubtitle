fbf4029a031e57c13e8784366ea20780ce1e5835
EDanilov <evg.danilov@gmail.com>
2021-12-11 14:56:09 +0300

    makeGui.bat cleanup

--- GUI/makeGui.bat
+++ GUI/makeGui.bat
@@ -1,5 +1,2 @@
 pyside2-uic MainWindow.ui -o MainWindow.py
-pyside2-uic SubtitlePreferencesWindow.ui -o SubtitlePreferencesWindow.py
-pyside2-uic TextExplorerExample.ui -o TextExplorerExample.py
-pyside2-uic TableSub.ui -o TableSub.py
 Pause
